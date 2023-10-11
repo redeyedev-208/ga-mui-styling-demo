874e3e37b8d112c65314ea91e494f94593bf928e
gilaguilar <redeyedev.208@gmail.com>
2023-10-10 18:39:14 -0600

    implemented basic table layout

--- src/components/Table/ContactTable.tsx
+++ src/components/Table/ContactTable.tsx
@@ -12,7 +12,39 @@ import { contactData } from '../../Data/ContactData';
 type Props = {};
 
 const ContactTable = (props: Props) => {
-  return <div>ContactTable</div>;
+  return (
+    <TableContainer>
+      {/* Table is based on rows and not columns in the data grid */}
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Name</TableCell>
+            <TableCell>Role</TableCell>
+            <TableCell>Skills</TableCell>
+            <TableCell>Start Date</TableCell>
+            <TableCell>Preference</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {contactData.map((contact) => {
+            return (
+              <TableRow key={contact.name}>
+                {
+                  // Short hand for giving us access to the key and the value
+                  Object.entries(contact).map(([key, value]) => {
+                    if (key !== 'id') {
+                      return <TableCell>{value}</TableCell>;
+                    }
+                    return '';
+                  })
+                }
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 };
 
 export default ContactTable;
